9cdb3d9f7dbf5ad37b15783daef48c3e941342c5
andrew.saenkov <andrew.saenkov@aurea.com>
2018-02-08 18:13:52 +0300

    BINANCE: review changes

--- src/main/java/com/gtc/tradinggateway/config/BinanceConfig.java
+++ src/main/java/com/gtc/tradinggateway/config/BinanceConfig.java
@@ -1,16 +1,9 @@
 package com.gtc.tradinggateway.config;
 
-import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
-import com.google.common.collect.ImmutableList;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.client.RestTemplate;
 
 import static com.gtc.tradinggateway.config.Const.CONF_ROOT_CHILD;
 import static com.gtc.tradinggateway.config.Const.Clients.BINANCE;
@@ -25,15 +18,7 @@ import static com.gtc.tradinggateway.config.Const.Clients.BINANCE;
 public class BinanceConfig extends BaseConfig {
 
     public BinanceConfig() {
-        mapper = new ObjectMapper(new JsonFactory())
-                .setPropertyNamingStrategy(PropertyNamingStrategy.LOWER_CAMEL_CASE)
-                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-                .configure(DeserializationFeature.FAIL_ON_NUMBERS_FOR_ENUMS, false)
-                .configure(DeserializationFeature.ACCEPT_FLOAT_AS_INT, true)
-                .configure(DeserializationFeature.FAIL_ON_NULL_CREATOR_PROPERTIES, false);
-
-        restTemplate = new RestTemplate(ImmutableList.of(
-                new MappingJackson2HttpMessageConverter(mapper)
-        ));
+        mapper = ConfigFactory.defaultMapper();
+        restTemplate = ConfigFactory.defaultRestTemplate(mapper);
     }
 }
---
+++ src/main/java/com/gtc/tradinggateway/config/ConfigFactory.java
@@ -0,0 +1,30 @@
+package com.gtc.tradinggateway.config;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.google.common.collect.ImmutableList;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by mikro on 08.02.2018.
+ */
+public class ConfigFactory {
+
+    public static ObjectMapper defaultMapper() {
+        return new ObjectMapper(new JsonFactory())
+                .setPropertyNamingStrategy(PropertyNamingStrategy.LOWER_CAMEL_CASE)
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                .configure(DeserializationFeature.FAIL_ON_NUMBERS_FOR_ENUMS, false)
+                .configure(DeserializationFeature.ACCEPT_FLOAT_AS_INT, true)
+                .configure(DeserializationFeature.FAIL_ON_NULL_CREATOR_PROPERTIES, false);
+    }
+
+    public static RestTemplate defaultRestTemplate(ObjectMapper mapper) {
+        return new RestTemplate(ImmutableList.of(
+                new MappingJackson2HttpMessageConverter(mapper)));
+    }
+
+}
