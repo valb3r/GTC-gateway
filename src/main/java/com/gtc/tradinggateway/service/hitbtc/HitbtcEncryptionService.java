16e3b7c5d3676355659d99911982e98f5a9f0584
andrew.saenkov <andrew.saenkov@aurea.com>
2018-02-18 16:04:50 +0300

    HITBTC: review fixesx

--- src/main/java/com/gtc/tradinggateway/service/hitbtc/HitbtcEncryptionService.java
+++ src/main/java/com/gtc/tradinggateway/service/hitbtc/HitbtcEncryptionService.java
@@ -1,15 +1,11 @@
 package com.gtc.tradinggateway.service.hitbtc;
 
-import com.google.common.collect.ImmutableMap;
 import com.gtc.tradinggateway.config.HitbtcConfig;
 import com.gtc.tradinggateway.util.BasicHttpAuthHelper;
 import lombok.RequiredArgsConstructor;
-import org.apache.commons.codec.binary.Base64;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
-
-import java.util.Map;
 
 /**
  * Created by mikro on 12.02.2018.
@@ -20,17 +16,9 @@ public class HitbtcEncryptionService {
 
     private final HitbtcConfig cfg;
 
-    public Map<String, String> signingHeaders() {
-        String converted = BasicHttpAuthHelper.generateToken(cfg.getPublicKey(), cfg.getSecretKey());
-
-        return ImmutableMap.<String, String>builder()
-                .put(HttpHeaders.AUTHORIZATION, "Basic " + converted)
-                .build();
-    }
-
     public HttpHeaders restHeaders() {
         HttpHeaders headers = new HttpHeaders();
-        headers.setAll(signingHeaders());
+        headers.setAll(BasicHttpAuthHelper.generateToken(cfg.getPublicKey(), cfg.getSecretKey()));
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
         return headers;
     }
--- src/main/java/com/gtc/tradinggateway/util/BasicHttpAuthHelper.java
+++ src/main/java/com/gtc/tradinggateway/util/BasicHttpAuthHelper.java
@@ -1,15 +1,22 @@
 package com.gtc.tradinggateway.util;
 
+import com.google.common.collect.ImmutableMap;
 import org.apache.commons.codec.binary.Base64;
+import org.springframework.http.HttpHeaders;
+
+import java.util.Map;
 
 /**
  * Created by mikro on 15.02.2018.
  */
 public class BasicHttpAuthHelper {
 
-    public static String generateToken(String username, String password) {
+    public static Map<String, String> generateToken(String username, String password) {
         String key = username + ":" + password;
-        return Base64.encodeBase64String(key.getBytes());
+        String encoded = Base64.encodeBase64String(key.getBytes());
+        return ImmutableMap.<String, String>builder()
+                .put(HttpHeaders.AUTHORIZATION, "Basic " + encoded)
+                .build();
     }
 
 }
