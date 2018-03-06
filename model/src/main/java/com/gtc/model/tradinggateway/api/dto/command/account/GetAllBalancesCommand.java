caca01aadcb87fe54f59f1840455d996881a61ed
Valentyn Berezin <valentyn.berezin@aurea.com>
2018-03-06 08:51:16 +0200

    Merge commit '989f2af8484fed4a579d25d2bf518f77b676355f'
    # Conflicts:
    #	model/src/main/java/com/gtc/model/tradinggateway/api/dto/command/account/GetAllBalancesCommand.java
    #	model/src/main/java/com/gtc/model/tradinggateway/api/dto/data/OrderDto.java
    #	model/src/main/java/com/gtc/model/tradinggateway/api/dto/response/withdraw/WithdrawOrderResponse.java

--- model/src/main/java/com/gtc/model/tradinggateway/api/dto/command/account/GetAllBalancesCommand.java
+++ model/src/main/java/com/gtc/model/tradinggateway/api/dto/command/account/GetAllBalancesCommand.java
@@ -1,10 +1,7 @@
 package com.gtc.model.tradinggateway.api.dto.command.account;
 
 import com.gtc.model.tradinggateway.api.dto.AbstractMessage;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 /**
  * Created by Valentyn Berezin on 03.03.18.
@@ -18,6 +15,11 @@ public class GetAllBalancesCommand extends AbstractMessage {
     private static final String HEADER = "getAllBalances";
 
     public static final String SELECTOR = HEADER_NAME + "='" + HEADER + "'";
+
+    @Builder
+    public GetAllBalancesCommand(String clientName, String id) {
+        super(clientName, id);
+    }
 
     @Override
     protected String getHeader() {
