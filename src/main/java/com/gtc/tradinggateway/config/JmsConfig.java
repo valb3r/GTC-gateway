9374afaaa60f36d22ebede635f79f5ebbd8673d3
Valentyn Berezin <valentyn.berezin@aurea.com>
2018-03-30 12:08:19 +0300

    Enabled ArtemisMQ instead of ActiveMQ

--- src/main/java/com/gtc/tradinggateway/config/JmsConfig.java
+++ src/main/java/com/gtc/tradinggateway/config/JmsConfig.java
@@ -2,13 +2,9 @@ package com.gtc.tradinggateway.config;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.jms.activemq.ActiveMQAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.jms.annotation.EnableJms;
-import org.springframework.jms.config.SimpleJmsListenerContainerFactory;
-
-import javax.jms.ConnectionFactory;
 
 /**
  * Created by Valentyn Berezin on 20.02.18.
@@ -18,13 +14,4 @@ import javax.jms.ConnectionFactory;
 @Import(ActiveMQAutoConfiguration.class)
 @ConditionalOnProperty("ESB_AMQ_ADDRESS")
 public class JmsConfig {
-
-    public static final String INBOUND = "INBOUND";
-
-    @Bean(name = INBOUND)
-    public SimpleJmsListenerContainerFactory inboundFactory(ConnectionFactory connectionFactory) {
-        SimpleJmsListenerContainerFactory inboundFactory = new SimpleJmsListenerContainerFactory();
-        inboundFactory.setConnectionFactory(connectionFactory);
-        return inboundFactory;
-    }
 }
