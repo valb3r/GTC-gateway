9857fbbcf14958a44b9dddf41c84532f516767db
Valentyn Berezin <valentyn.berezin@aurea.com>
2018-04-03 10:10:06 +0300

    Add JMS tweaks

--- src/main/java/com/gtc/tradinggateway/config/JmsConfig.java
+++ src/main/java/com/gtc/tradinggateway/config/JmsConfig.java
@@ -1,10 +1,17 @@
 package com.gtc.tradinggateway.config;
 
+import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.jms.activemq.ActiveMQAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
 import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.connection.CachingConnectionFactory;
+
+import javax.jms.ConnectionFactory;
 
 /**
  * Created by Valentyn Berezin on 20.02.18.
@@ -14,4 +21,13 @@ import org.springframework.jms.annotation.EnableJms;
 @Import(ActiveMQAutoConfiguration.class)
 @ConditionalOnProperty("ESB_AMQ_ADDRESS")
 public class JmsConfig {
+
+    @Bean
+    @Primary
+    public ConnectionFactory connectionFactory(@Value("${spring.artemis.pool}") int poolSize,
+                                               ActiveMQConnectionFactory connectionFactory) {
+        CachingConnectionFactory factory = new CachingConnectionFactory(connectionFactory);
+        factory.setSessionCacheSize(poolSize);
+        return factory;
+    }
 }
